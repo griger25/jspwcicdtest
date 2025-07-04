c1c221d86663ef513a4e7465f98b5c19fbec39c7
GermanGri <german.gribanov@gmail.com>
2025-07-04 11:09:04 +0300

    тест

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title test', async ({ page }) => {
+test('has title test111', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
